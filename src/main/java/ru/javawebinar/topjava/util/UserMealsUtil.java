51ee287ff7fa3520665e4ab985267caed34aa243
kate <kate.krivikh@gmail.com>
2020-05-26 16:27:44 +0300

    HW0_removing_filteredByCollector

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -8,7 +8,6 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.*;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class UserMealsUtil {
@@ -27,8 +26,6 @@ public class UserMealsUtil {
         mealsTo.forEach(System.out::println);
 
         System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
-
-        System.out.println(filteredByCollector(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
@@ -60,59 +57,6 @@ public class UserMealsUtil {
                     return createUserMealWithExcess(e, excess);
                 })
                 .collect(Collectors.toList());
-    }
-
-    // TODO I do not extract this collector in separate class because I don't think my solution is good.
-    // TODO Once I had another task like this one, but could not release collector.
-    // TODO Other words: this is my first Collector, I expect your comments, please.
-    public static List<UserMealWithExcess> filteredByCollector(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        Collector<UserMeal, Map<LocalDate, Map<UserMeal, Integer>>, List<UserMealWithExcess>> collector = Collector.of(
-                TreeMap::new,
-                (map, userMeal) -> {
-                    LocalDate localDate = getDate(userMeal);
-
-                    // meals for concrete day, value is the same for all keys
-                    Map<UserMeal, Integer> mealsWithTotalDayCalories = map.getOrDefault(localDate, new HashMap<>());
-
-                    // forming total calories for concrete day: current + all previous
-                    int totalDayCalories = userMeal.getCalories();
-                    Iterator<Integer> iterator = mealsWithTotalDayCalories.values().iterator();
-                    // only first, all values are the same
-                    if (iterator.hasNext())
-                        totalDayCalories += iterator.next();
-
-                    // refresh all previous meals
-                    for (Map.Entry<UserMeal, Integer> entry : mealsWithTotalDayCalories.entrySet()) {
-                        entry.setValue(totalDayCalories);
-                    }
-                    // add current meal
-                    mealsWithTotalDayCalories.put(userMeal, totalDayCalories);
-
-                    map.put(localDate, mealsWithTotalDayCalories);
-                },
-                (l, r) -> {
-                    l.putAll(r);
-                    return l;
-                },
-                (m) -> {
-                    List<UserMealWithExcess> resultList = new ArrayList<>();
-                    for (Map<UserMeal, Integer> mealsWithTotalDayCalories : m.values()) {
-                        // in this cycle count of entries will be 3-5, don't think than complexity is O(N*N): O(N*5) ~ O(N)
-                        for (Map.Entry<UserMeal, Integer> currentMealWithTotalDayCalories : mealsWithTotalDayCalories.entrySet()) {
-                            UserMeal meal = currentMealWithTotalDayCalories.getKey();
-                            // TODO i don't like using the "startTime", "endTime" and "caloriesPerDay" directly here.
-                            // TODO But i don't know how to do better
-                            if (TimeUtil.isBetweenHalfOpen(getTime(meal), startTime, endTime)) {
-                                boolean excess = isExcess(currentMealWithTotalDayCalories.getValue(), caloriesPerDay);
-                                UserMealWithExcess userMealWithExcess = createUserMealWithExcess(meal, excess);
-                                resultList.add(userMealWithExcess);
-                            }
-                        }
-                    }
-                    return resultList;
-                });
-
-        return meals.stream().collect(collector);
     }
 
 
