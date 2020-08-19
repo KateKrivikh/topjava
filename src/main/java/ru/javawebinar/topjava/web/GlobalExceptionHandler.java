8c7b3eda0ae0286e3ea3e4e0830022d0b52ee3f0
kate <kate.krivikh@gmail.com>
2020-08-19 17:31:27 +0300

    HW10_rollback changings

--- src/main/java/ru/javawebinar/topjava/web/GlobalExceptionHandler.java
+++ src/main/java/ru/javawebinar/topjava/web/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,26 +16,14 @@ import java.util.Map;
 public class GlobalExceptionHandler {
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ModelAndView conflict(HttpServletRequest req, DataIntegrityViolationException e) throws Exception {
-        log.error("Exception at request " + req.getRequestURL(), e);
-        Throwable modified = ValidationUtil.transformExceptionIfUniqueKeyDuplication(e);
-
-        return createExceptionView(modified, HttpStatus.CONFLICT);
-    }
-
     @ExceptionHandler(Exception.class)
     public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
         log.error("Exception at request " + req.getRequestURL(), e);
         Throwable rootCause = ValidationUtil.getRootCause(e);
 
-        return createExceptionView(rootCause, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    private ModelAndView createExceptionView(Throwable e, HttpStatus httpStatus) {
+        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
         ModelAndView mav = new ModelAndView("exception",
-                Map.of("exception", e, "message", e.toString(), "status", httpStatus));
-
+                Map.of("exception", rootCause, "message", rootCause.toString(), "status", httpStatus));
         mav.setStatus(httpStatus);
 
         // Interceptor is not invoked, put userTo
@@ -44,7 +31,6 @@ public class GlobalExceptionHandler {
         if (authorizedUser != null) {
             mav.addObject("userTo", authorizedUser.getUserTo());
         }
-
         return mav;
     }
 }
